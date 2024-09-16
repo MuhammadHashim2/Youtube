e60249c676ba498b8267a37f9150e35e7cd6f817
Muhammad Hashim <muhammadhashim10037@gmail.com>
2024-09-16 23:15:53 +0500

    Add files via upload

---
+++ JS/app.js
@@ -0,0 +1,16 @@
+  function Cncolor1(){addEventListener("click",()=>{
+    document.querySelector(".one").style.backgroundColor="black";
+    document.querySelector(".one").style.color="white";
+})}
+function Cncolor2(){addEventListener("click",()=>{
+    document.querySelector(".two").style.backgroundColor="black";
+    document.querySelector(".two").style.color="white";
+})}
+function Cncolor3(){addEventListener("click",()=>{
+    document.querySelector(".three").style.backgroundColor="black";
+    document.querySelector(".three").style.color="white";
+})}
+function Cncolor4(){addEventListener("click",()=>{
+    document.querySelector(".four").style.backgroundColor="black";
+    document.querySelector(".four").style.color="white";
+})}
